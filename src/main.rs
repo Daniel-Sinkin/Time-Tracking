4bcf0bec3973535a64b986fc4aab5d5dbe674ab5
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-23 07:14:55 +0200

    Continued work on importing CSV

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,48 @@
-use clap::Parser;
+use time::{Date, Time};
 
-#[derive(Parser)]
-#[command(name = "MyApp")]
-#[command(author = "Kevin K. <kbknapp@gmail.com>")]
-#[command(version = "1.0")]
-#[command(about = "Does awesome things", long_about = None)]
-struct Cli {
-    #[arg(long)]
-    two: String,
-    #[arg(long)]
-    one: String,
+use csv::Reader;
+
+use std::fs::File;
+
+use strum_macros::EnumString;
+
+#[derive(strum::Display, EnumString)]
+enum Category {
+    Programming,
+    Mathematics
+}
+
+#[derive(strum::Display, EnumString)]
+enum Tag { Rust, Cpp, Python, Statistics, Finance }
+
+struct TimeEntry {
+    category : Category,
+    tags : Vec<Tag>,
+    start_date : Date,
+    start_time : Time,
+    end_date : Date,
+    end_time : Time,
+    description : String
 }
 
 fn main() {
-    let cli = Cli::parse();
+    println!("Hello, World!");
 
-    println!("two: {:?}", cli.two);
-    println!("one: {:?}", cli.one);
+    // Category, Tags, Task, startDate, startTime, endDate, endTime, Description / Notes
+
+    let path = "Data/Clockify.csv";
+    let file = File::open(path).unwrap();
+
+    let mut reader = Reader::from_reader(file);
+
+    for result in reader.records() {
+        match result {
+            Ok(record) => {
+                for (idx, field) in record.iter().enumerate() {
+                    println!("Field {}: {}", idx, field);
+                }
+            },
+            Err(err) => println!("Error reading CSV record: {}", err),
+        }
+    }
 }
