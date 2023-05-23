cb78a6246cce8b3f6a79725238455d2a6defcfa9
Daniel-Sinkin <Daniel.Sinkin@gmx.de>
2023-05-23 08:06:25 +0200

    Finished the reading of the CSV, the code is terrible right now, especially there is absolutely no error handling of any kind. If I assume that the Clockify Outputs will always be perfectly formatted then this is okay, because I only need to read one particular type of .csv file instead of random data, although it is not a good habit to get into, so I might fix it up later anyways. But we'll see, for now the input works, I need to clean up the code at least a little bit, maybe put the IO into a seperate file, after all I also want to be able to save my Time Entries to my own thing. Maybe I'll even make it so I can push them back to Clockify, but not doing that right now. Now that the file reading is done I can get to the actual time tracking, so the next step is to write a CLI interface to start and stop tracking time, and then appending this to a file, so [ ] CLI interface, [ ] Writing to File. The step after that is either a UI, connecting to a SQL database or data visualization

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-use time::{Date, Time};
+use time::{Date, Time, Month};
 
 use csv::Reader;
 
-use std::fs::File;
+use std::{fs::File, str::FromStr};
 
 use strum_macros::EnumString;
 
@@ -25,6 +25,50 @@ struct TimeEntry {
     description : String
 }
 
+fn parse_month(month_str: &str) -> Option<Month> {
+    match month_str {
+        "01" => Some(Month::January),
+        "02" => Some(Month::February),
+        "03" => Some(Month::March),
+        "04" => Some(Month::April),
+        "05" => Some(Month::May),
+        "06" => Some(Month::June),
+        "07" => Some(Month::July),
+        "08" => Some(Month::August),
+        "09" => Some(Month::September),
+        "10" => Some(Month::October),
+        "11" => Some(Month::November),
+        "12" => Some(Month::December),
+        _ => None,
+    }
+}
+
+fn string_to_time(str: String) -> Time {
+    // This only works if the string is formatted as HH:MM:SS
+    let str_hrs = &str[0..=1];
+    let str_min = &str[3..=4];
+    let str_sec = &str[6..=7];
+
+    Time::from_hms(str_hrs.parse::<u8>().unwrap(),
+        str_min.parse::<u8>().unwrap(),
+        str_sec.parse::<u8>().unwrap()).unwrap()
+}
+
+fn string_to_date(str: String) -> Date {
+    // This only works if the string is formatted as dd/mm/yyyy
+    let str_day = &str[0..=1];
+    let day = str_day.parse::<u8>().unwrap();
+    let str_month = &str[3..=4];
+    let month = if let Some(y) = parse_month(str_month) {
+        y
+    } else {
+        panic!();
+    };
+    let str_year = &str[6..=9];
+    let year = str_year.parse::<i32>().unwrap();
+    Date::from_calendar_date(year, month, day).unwrap()
+}
+
 fn main() {
     println!("Hello, World!");
 
@@ -35,14 +79,23 @@ fn main() {
 
     let mut reader = Reader::from_reader(file);
 
-    for result in reader.records() {
-        match result {
-            Ok(record) => {
-                for (idx, field) in record.iter().enumerate() {
-                    println!("Field {}: {}", idx, field);
-                }
-            },
-            Err(err) => println!("Error reading CSV record: {}", err),
+    let mut is_header = true;
+
+    while let Some(x) = reader.records().next() {
+        if is_header {
+            is_header = false;
+        } else {
+            if let Ok(y) = x {
+                let te = TimeEntry{
+                    category: Category::from_str(&y[0]).unwrap(),
+                    tags: vec![],
+                    start_date: string_to_date(String::from(&y[9])),
+                    start_time: string_to_time(String::from(&y[10])),
+                    end_date: string_to_date(String::from(&y[11])),
+                    end_time: string_to_time(String::from(&y[12])),
+                    description: String::from(""),
+                };
+            }
         }
     }
 }
